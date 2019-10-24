72b79a200ecb0e96c0d4be68944663216ab82412
Ben Standerline <ben.standerline@italik.co.uk>
2019-10-24 16:20:44 +0100

    Add http function for login.html

--- start.go
+++ start.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -20,7 +21,9 @@ func main() {
 
 	port := ":80"
 
-	http.HandleFunc("/", homepage)
+	http.HandleFunc("/", login)
+	http.HandleFunc("/login.html", loginhtml)
+	http.HandleFunc("/login.css", logincss)
 	err = http.ListenAndServe(":80", nil)
 	if err != nil {
 		log.Fatalln(err)
@@ -28,6 +31,22 @@ func main() {
 	fmt.Println("Listening on " + port)
 }
 
-func homepage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, world!")
+// Needs to be modified to check for a valid token - if none it should launch straight into the dashboard
+func login(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/login.html", http.StatusSeeOther)
+}
+
+func loginhtml(w http.ResponseWriter, r *http.Request) {
+	contents, err := ioutil.ReadFile("www/login/login.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.Write(contents)
+}
+func logincss(w http.ResponseWriter, r *http.Request) {
+	contents, err := ioutil.ReadFile("www/login/login.css")
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.Write(contents)
 }
