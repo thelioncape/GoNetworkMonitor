45875e94bf744353473dff059c9262563d2f2ba9
Ben Standerline <ben.standerline@italik.co.uk>
2019-10-28 13:30:27 +0000

    Create credentials struct and CheckCreds function

--- modules/authentication/authentication.go
+++ modules/authentication/authentication.go
@@ -6,6 +6,12 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
+// Credentials is a type that represents a user's credentials (hashed password)
+type Credentials struct {
+	username   string
+	hashedpass string
+}
+
 // HashPass hashes the given password using the cost defined in this function and returns the hash as []byte
 func HashPass(password []byte) []byte {
 	cost := 10
@@ -14,4 +20,8 @@ func HashPass(password []byte) []byte {
 		log.Fatal(err)
 	}
 	return hash
+}
+
+func CheckCreds(creds Credentials) bool {
+	return true
 }
