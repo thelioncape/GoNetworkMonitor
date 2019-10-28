0b00a68d5352646b2108b998a5732b47124a3662
Ben Standerline <46759388+thelioncape@users.noreply.github.com>
2019-10-28 13:53:52 +0000

    Prototype SQLite user management functions

---
+++ modules/authentication/sqliteauth.go
@@ -0,0 +1,21 @@
+package authentication
+
+func GetSQLiteUser(username string) Credentials {
+
+}
+
+func CreateSQLiteUser(creds Credentials) bool {
+
+}
+
+func DeleteSQLiteUser(username string) bool {
+
+}
+
+func ResetSQLiteUser(username string, passhash []byte) Credentials {
+
+}
+
+func CheckSQLiteUser(creds Credentials) bool {
+
+}
